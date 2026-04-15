70399a24d7b0acc07351a0178180c33151f4bdb3
Mansyahariii <arifirmansyah350@gmail.com>
2026-04-15 21:03:21 +0700

    feat: implement product listing in main
    menghubungkan ProductProvider dengan UI utama
    menampilkan data produk dalam bentuk grid
    menangani state loading, error, dan success
    menambahkan tombol retry saat gagal memuat data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:market_place/features/dashboard/presentation/providers/product_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +11,38 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final product = context.watch<ProductProvider>();
+
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: switch (product.status) {
+          // Tampilkan spinner saat loading
+          ProductStatus.loading || ProductStatus.initial => const Center(
+            child: CircularProgressIndicator(),
+          ),
+
+          // Tampilkan pesan error + tombol retry
+          ProductStatus.error => Center(
+            child: Column(
+              children: [
+                Text(product.error ?? 'Error'),
+                ElevatedButton(
+                  onPressed: () => product.fetchProducts(),
+                  child: const Text('Coba Lagi'),
+                ),
+              ],
+            ),
+          ),
+
+          // Tampilkan grid produk
+          ProductStatus.loaded => GridView.builder(
+            itemCount: product.products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (_, i) => _ProductCard(product: product.products[i]),
+          ),
+        },
       ),
     );
   }
