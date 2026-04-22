7c9da494d3f66fbbfc3e65212d84efed482563fa
Mansyahariii <arifirmansyah350@gmail.com>
2026-04-22 16:59:31 +0700

    fix: remove BOM character from import in auth pages
    Menghapus karakter BOM (\uFEFF) yang muncul di baris pertama import pada login_page.dart dan register_page.dart untuk mencegah potensi masalah parsing compiler.

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:email_validator/email_validator.dart';
+﻿import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
